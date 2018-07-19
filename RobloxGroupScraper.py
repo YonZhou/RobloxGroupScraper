194e76afd2181bd6b40d8a88b5c42038f7b8d500
YonZhou <yonzhouschool@gmail.com>
2018-07-18 20:03:57 -0700

    removed the sleep function for more reliable wait
    added webdriverwait for page number to load and added a loop that keeps updating the page until groupmember classes are found(loaded)

--- RobloxGroupScraper.py
+++ RobloxGroupScraper.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
 
@@ -10,7 +12,7 @@ i = 1
 
 
 browser = webdriver.Chrome()
-browser.get("https://www.roblox.com/Groups/Group.aspx?gid=3336691")
+browser.get("https://www.roblox.com/groups/group.aspx?gid=4097667")
 
 src = BeautifulSoup(browser.page_source ,"html.parser")
 pageNums = int(src.find("div",attrs={"class":"paging_pagenums_container"}).text)
@@ -19,11 +21,18 @@ while i <= pageNums:
     #pageEnter = browser.find_element_by_id('ctl00_cphRoblox_rbxGroupRoleSetMembersPane_dlUsers_Footer')
     #pageEnter.click()
     pageEnter = browser.find_element_by_id('ctl00_cphRoblox_rbxGroupRoleSetMembersPane_dlUsers_Footer_ctl01_PageTextBox')
+    WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.ID, 'ctl00_cphRoblox_rbxGroupRoleSetMembersPane_dlUsers_Footer_ctl01_PageTextBox')))
     pageEnter.clear()
     pageEnter.send_keys(str(i))
     pageEnter.send_keys(Keys.RETURN)
+    #time.sleep(2)
+    temp = []
+    while len(temp) < 1:
+        src = BeautifulSoup(browser.page_source ,"html.parser")
+        temp = src.findAll("div",attrs={"class":"GroupMember"})
+        print (len(temp))
 
-    time.sleep(2)
+    temp.clear()
 
     src = BeautifulSoup(browser.page_source ,"html.parser")
     membersClass = src.findAll("div",attrs={"class":"GroupMember"})
